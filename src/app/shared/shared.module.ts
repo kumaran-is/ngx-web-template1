8bcb69428c2a5a8b60a410dc2cf7e8287dae42d8
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2019-03-08 08:06:58 -0500

    added custom attribute directive for mouse hove highlight the element with color

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HoverDirective } from './directives/hover.directive';
 import { MaterialModule } from './material/material.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [HoverDirective],
   imports: [CommonModule, ReactiveFormsModule, MaterialModule],
-  exports: [CommonModule, ReactiveFormsModule, MaterialModule]
+  exports: [CommonModule, ReactiveFormsModule, MaterialModule, HoverDirective]
 })
 export class SharedModule {}
