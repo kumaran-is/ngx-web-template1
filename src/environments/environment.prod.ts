cd9dd274acf60ad1fa2de7ecbe7dc487196078a3
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2019-03-07 22:41:33 -0500

    updated version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   webAPIUrl: 'https://mydomain.com/api/v1',
   // version is automatically updated during build from package.json
-  version: '0.0.3',
+  version: '0.0.4',
   configFilePath: 'assets/config/prod.config.json'
 };
 
