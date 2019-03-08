fd9f9cff43a5bff2eb6806934ff9c330bae7167e
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2019-03-08 08:55:28 -0500

    common api service

--- src/app/core/api/api.service.ts
+++ src/app/core/api/api.service.ts
@@ -9,19 +9,24 @@ import { Observable } from 'rxjs';
 export class APIService {
   constructor(private http: HttpClient) {}
 
-  public get(
-    path: string,
-    params: HttpParams = new HttpParams()
-  ): Observable<any> {
+  public get(path: string, params?: any): Observable<any> {
     return this.http.get(
-      this.createCompleteURLPath(environment.webAPIUrl, path),
-      { params }
+      this.createCompleteURLPath(path),
+      this.generateQueryString(params)
+    );
+  }
+
+  public patch(path: string, body): Observable<any> {
+    return this.http.patch(
+      this.createCompleteURLPath(path),
+      body,
+      this.generateHeaders()
     );
   }
 
   public post(path: string, body): Observable<any> {
     return this.http.post(
-      this.createCompleteURLPath(environment.webAPIUrl, path),
+      this.createCompleteURLPath(path),
       body,
       this.generateHeaders()
     );
@@ -29,25 +34,38 @@ export class APIService {
 
   public put(path: string, body): Observable<any> {
     return this.http.put(
-      this.createCompleteURLPath(environment.webAPIUrl, path),
+      this.createCompleteURLPath(path),
       body,
       this.generateHeaders()
     );
   }
 
   public delete(path: string): Observable<any> {
-    return this.http.delete(
-      this.createCompleteURLPath(environment.webAPIUrl, path)
-    );
+    return this.http.delete(this.createCompleteURLPath(path));
   }
 
-  private createCompleteURLPath(envAPIUrl: string, path: string) {
-    return `${envAPIUrl}/${path}`;
+  private createCompleteURLPath(path: string) {
+    return `${environment.webAPIUrl}/${path}`;
   }
 
   private generateHeaders() {
     return {
       headers: new HttpHeaders({ 'Content-Type': 'application/json' })
     };
+  }
+
+  private generateQueryString(params?: any) {
+    let queryString;
+    if (params) {
+      queryString = {
+        params: new HttpParams()
+      };
+      for (const param in params) {
+        if (params.hasOwnProperty(param)) {
+          queryString.params.set(param, params[param]);
+        }
+      }
+    }
+    return queryString;
   }
 }
