f4780a527ca6c5d01c76ec1f60a2d60188b6344e
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2019-03-14 23:31:07 -0400

    added auth guard & auth service

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -9,12 +9,15 @@ import {
   UrlSegment,
   UrlTree
 } from '@angular/router';
+import { AuthService } from '@core/auth/auth.service';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
+  constructor(private authService: AuthService) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -23,8 +26,10 @@ export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    return true;
+    console.log('In canActivate: ' + state.url);
+    return this.isUserLoggedIn(state.url);
   }
+
   canActivateChild(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -33,12 +38,29 @@ export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    return true;
+    console.log('In canActivateChild: ' + state.url);
+    return this.isUserLoggedIn(state.url);
   }
+
   canLoad(
     route: Route,
     segments: UrlSegment[]
   ): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+    console.log('In canLoad: ' + route.path);
+    return this.isUserLoggedIn(route.path);
+  }
+
+  private isUserLoggedIn(url: string): Promise<boolean> {
+    return new Promise(resolve => {
+      if (1 === 1) {
+        resolve(true);
+      } else {
+        // Retain the attempted URL for redirection after successful login
+        // this.authService.setRedirectUrl(url);
+        // redirect to login page
+        // this.dialogService.popupDialog('signin');
+        resolve(false);
+      }
+    });
   }
 }
