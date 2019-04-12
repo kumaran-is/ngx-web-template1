18ad02a55285c76d3213e73d3b39c8140522674f
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2019-04-11 23:47:48 -0400

    added signup components

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -3,15 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { ForgotPasswordComponent } from '@app/authentication/forgot-password/forgot-password.component';
 import { LoginComponent } from '@app/authentication/login/login.component';
 import { LogoutComponent } from '@app/authentication/logout/logout.component';
-import { RegisterComponent } from '@app/authentication/register/register.component';
 import { ResetPasswordComponent } from '@app/authentication/reset-password/reset-password.component';
+import { SignupComponent } from '@app/authentication/signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'logout', component: LogoutComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'sign-up', component: SignupComponent },
   { path: 'reset-password', component: ResetPasswordComponent }
 ];
 
@@ -24,7 +24,7 @@ export class AuthenticationRoutingModule {
     ForgotPasswordComponent,
     LoginComponent,
     LogoutComponent,
-    RegisterComponent,
-    ResetPasswordComponent
+    ResetPasswordComponent,
+    SignupComponent
   ];
 }
