3010ba177b16394b0eeca700354e9236c6b8fb5c
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2019-05-17 15:14:24 -0400

    added font display

---
+++ src/environments/environment.latest.ts
@@ -0,0 +1,29 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  webAPIUrl: 'http://localhost:8000/api/v1',
+  // version is automatically updated during build from package.json
+  version: '0.0.10',
+  configFilePath: 'assets/config/latest.config.json',
+  envName: 'LATEST',
+  firestore: {
+    apiKey: 'AIzaSyDiRtJO3vLAn_I7tVdZMjddeh8Dyz-_vmY',
+    authDomain: 'halls-diary.firebaseapp.com',
+    databaseURL: 'https://halls-diary.firebaseio.com',
+    projectId: 'halls-diary',
+    storageBucket: 'halls-diary.appspot.com',
+    messagingSenderId: '670696523764'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
