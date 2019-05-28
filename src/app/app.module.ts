2f050b5f12bbf66dafe15bd93d2bc1017aa7d744
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2019-05-28 16:08:52 -0400

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,10 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Router } from '@angular/router';
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/app.component';
-import { DialogService } from '@app/auth/services/dialog.service';
 import { HomeModule } from '@app/home/home.module';
-import { RootStoreModule } from '@app/root-store';
-import {
-  AppInitStoreFacade,
-  initApplication
-} from '@app/root-store/app-init/app-init-store.facade';
 import { CoreModule } from '@core/core.module';
 import { LayoutModule } from '@layout/layout.module';
 import { SharedModule } from '@shared/shared.module';
@@ -22,23 +16,11 @@ import { SharedModule } from '@shared/shared.module';
     BrowserAnimationsModule,
     CoreModule,
     SharedModule,
-    RootStoreModule,
     LayoutModule,
     HomeModule,
     AppRoutingModule
   ],
-  providers: [
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initApplication,
-      deps: [AppInitStoreFacade],
-      multi: true
-    },
-    {
-      provide: 'IDialog',
-      useClass: DialogService
-    }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
