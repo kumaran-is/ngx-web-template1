fa17b6487b90f15702b16e7608588a567f351adb
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2019-05-28 21:01:33 -0400

    implement page title service

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -7,7 +7,7 @@ import { Credential } from '@app/auth/models/credential.model';
 import { User } from '@app/auth/models/user.model';
 import * as firebase from 'firebase/app';
 import { Observable, of } from 'rxjs';
-import { first, map } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -179,11 +179,7 @@ export class AuthService extends StopSubscribe {
     userCredential: firebase.auth.UserCredential
   ): Promise<boolean> {
     return this.firestoreAPIService
-      .doc$(`/users/${userCredential.user.uid}/`)
-      .pipe(
-        first(),
-        map(Boolean)
-      )
+      .recordExists$(`/users/${userCredential.user.uid}/`)
       .toPromise();
   }
 
