1a3e66720a01a90494d4c1e35e4fd607be61c6be
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2019-06-02 23:30:34 -0400

    updated changelog and version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   webAPIUrl: 'http://localhost:8000/api/v1',
   // version is automatically updated during build from package.json
-  version: '0.0.16',
+  version: '0.0.17',
   configFilePath: 'assets/config/dev.config.json',
   envName: 'dev',
   firestore: {
